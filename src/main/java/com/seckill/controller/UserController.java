da09a0ea512c24c349111ea5e278ab12cafa52e0
TorresWang <t9_wangjy@qq.com>
2020-02-28 17:43:48 +0800

    modified:   .idea/workspace.xml 	new file:   note.txt 	modified:   src/main/java/com/seckill/controller/UserController.java 	modified:   target/classes/com/seckill/controller/UserController.class

--- src/main/java/com/seckill/controller/UserController.java
+++ src/main/java/com/seckill/controller/UserController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 /**
  * Created by admin on 2020/2/19.
@@ -25,6 +26,31 @@ public class UserController extends BaseController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private HttpServletRequest httpServletRequest;
+
+    //用户获取otp短信接口
+    @RequestMapping("/getOtp")
+    @ResponseBody
+    public CommonReturnType getOtp(@RequestParam(name = "telephone") String telephone){
+        //需要按照一定的规则生产OTP验证码
+        Random random = new Random();
+        int randomInt = random.nextInt(99999);//此时随机数取值[0,99999)
+        randomInt += 10000;
+        String otpCode = String.valueOf(randomInt);
+
+        //将OTP验证码同对应用户的手机号关联，企业级应用会做到redis中。
+        // 此处使用httpSession的方式绑定用户的手机号与OTPCODE
+
+
+        //将otp验证码通过短信通道发送给用户，此项目省略
+
+        return CommonReturnType.create(null);
+    }
+
+
+
+
     @RequestMapping("/get")
     @ResponseBody
     public CommonReturnType getUser(@RequestParam(name="id")Integer id) throws BusinessException {
@@ -32,7 +58,7 @@ public class UserController extends BaseController {
         UserModel userModel = userService.getUserById(id);
 
         if(userModel == null){
-            throw new BusinessException(EmBusinessError.USER_NOT_EXIST);
+            throw new BusinessException(EmBusinessError.PARAMETER_VALIDATION_ERROR,"手机号码不匹配");
         }
 
         //将核心领域模型用户对象转化为可供UI使用的viewoject
